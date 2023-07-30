e5e01b9b156ad61026dd73424b6a61919c9aa400
Mycroft <mycroft.tovarish@gmail.com>
2023-07-30 00:55:43 -0700

    Update synched.c
    fix name overflow

--- blakserv/synched.c
+++ blakserv/synched.c
@@ -203,7 +203,7 @@ void SynchedProtocolParse(session_node *s,client_msg *msg)
          break;
       if (len >= sizeof(name))
          break;
-      memcpy(name, msg->data + index + 2, len);
+      memcpy_s(name, sizof name, msg->data + index + 2, len);
       name[len] = 0; /* null terminate string */
       index += 2 + len;
       
